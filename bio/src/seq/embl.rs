c25d94c01b054d3f7b627d9fead28e296f980db8
Matthew Pocock <turingatemyhamster@gmail.com>
2019-10-08 19:34:22 +0100

    EMBL Location parsing works :)

--- bio/src/seq/embl.rs
+++ bio/src/seq/embl.rs
@@ -5,11 +5,14 @@ use nom::{
   },
   bytes::complete::{
     tag,
+    take_while1,
   },
-  character::complete::{ 
-    digit1,
-    alphanumeric1,
-    },
+  character::{
+    is_alphanumeric,
+    complete::{ 
+      digit1,
+    }
+  },
   combinator::{ 
     map,
     map_res,
@@ -108,6 +111,16 @@ enum Local {
   Span { from: Position, to: Position, before_from: bool, after_to: bool },
 }
 
+impl Local {
+  fn span(from: u32, to: u32) -> Local {
+    Local::Span {
+      from: Position::Point(Point(from)),
+      to: Position::Point(Point(to)),
+      before_from: false,
+      after_to: false }
+  }
+}
+
 fn parse_local(input: &str) -> IResult<&str, Local> {
   let parse_within = map(
     tuple((parse_point, tag("."), parse_point)),
@@ -124,10 +137,10 @@ fn parse_local(input: &str) -> IResult<&str, Local> {
   );
 
   alt((
-    map(parse_point, Local::Point),
     map(parse_between, Local::Between),
     parse_within,
-    parse_span
+    parse_span,
+    map(parse_point, Local::Point), // must do this last as it's a prefix of the others
   ))(input)
 }
 
@@ -139,9 +152,14 @@ enum Loc {
 }
 
 fn parse_loc(input: &str) -> IResult<&str, Loc> {
+  let parse_accession = take_while1(|c| {
+    let b = c as u8;
+    is_alphanumeric(b) || b == b'.'
+  });
+
   alt((
     map(
-      tuple((alphanumeric1, tag(":"), parse_local)),
+      tuple((parse_accession, tag(":"), parse_local)),
       |(within, _, at)| Loc::Remote { within: within.to_string(), at }
     ),
     map(parse_local, Loc::Local)
@@ -208,6 +226,7 @@ mod tests {
   fn test_parse_locations_from_spec() {
     let parse_locOp = |input| {
       match super::parse_locOp(input) {
+        Ok((rem, ref res)) if !rem.is_empty() => panic!("Non-empty remaining input {}, parsed out {:?}", rem, res),
         Ok((_, res)) => res,
         e => panic!("Problem parsing: {:?}", e)
       }
@@ -226,17 +245,76 @@ mod tests {
         after_to: false
         })));
 
-    parse_locOp("<345..500");
-    parse_locOp("<1..888");
-    parse_locOp("1..>888");
-    parse_locOp("102.110");
-    parse_locOp("123^124");
-    parse_locOp("join(12..78,134..202)");
-    parse_locOp("complement(34..126)");
-    parse_locOp("complement(join(2691..4571,4918..5163))");
-    parse_locOp("join(complement(4918..5163),complement(2691..4571))");
-    parse_locOp("J00194.1:100..202");
-    parse_locOp("join(1..100,J00194.1:100..202)");
+    assert_eq!(
+      parse_locOp("<345..500"),
+      LocOp::Loc(Loc::Local(Local::Span {
+        from: Position::Point(Point(345)),
+        to: Position::Point(Point(500)),
+        before_from: true,
+        after_to: false
+      })));
+    
+    assert_eq!(
+      parse_locOp("<1..888"),
+      LocOp::Loc(Loc::Local(Local::Span {
+        from: Position::Point(Point(1)),
+        to: Position::Point(Point(888)),
+        before_from: true,
+        after_to: false
+      })));
+
+    
+    assert_eq!(
+      parse_locOp("1..>888"),
+      LocOp::Loc(Loc::Local(Local::Span {
+        from: Position::Point(Point(1)),
+        to: Position::Point(Point(888)),
+        before_from: false,
+        after_to: true
+      })));
+
+    assert_eq!(
+      parse_locOp("102.110"),
+      LocOp::Loc(Loc::Local(Local::Within { from: Point(102), to: Point(110) })));
+
+    assert_eq!(
+      parse_locOp("123^124"),
+      LocOp::Loc(Loc::Local(Local::Between(Between(123, 124)))));
+    
+    assert_eq!(
+      parse_locOp("join(12..78,134..202)"),
+      LocOp::Join(vec![
+        LocOp::Loc(Loc::Local(Local::span(12, 78))),
+        LocOp::Loc(Loc::Local(Local::span(134, 202)))]));
+
+    assert_eq!(
+      parse_locOp("complement(34..126)"),
+      LocOp::Complement(Box::new(LocOp::Loc(Loc::Local(Local::span(34, 126))))));
+    
+    assert_eq!(
+      parse_locOp("complement(join(2691..4571,4918..5163))"),
+      LocOp::Complement(Box::new(LocOp::Join(vec![
+        LocOp::Loc(Loc::Local(Local::span(2691, 4571))),
+        LocOp::Loc(Loc::Local(Local::span(4918, 5163)))
+      ]))));
+    
+    assert_eq!(
+      parse_locOp("join(complement(4918..5163),complement(2691..4571))"),
+      LocOp::Join(vec![
+        LocOp::Complement(Box::new(LocOp::Loc(Loc::Local(Local::span(4918, 5163))))),
+        LocOp::Complement(Box::new(LocOp::Loc(Loc::Local(Local::span(2691, 4571)))))
+      ]));
+
+    assert_eq!(
+      parse_locOp("J00194.1:100..202"),
+      LocOp::Loc(Loc::Remote{ within: String::from("J00194.1"), at: Local::span(100, 202) }));
+    
+    assert_eq!(
+      parse_locOp("join(1..100,J00194.1:100..202)"),
+      LocOp::Join(vec![
+        LocOp::Loc(Loc::Local(Local::span(1, 100))),
+        LocOp::Loc(Loc::Remote { within: String::from("J00194.1"), at: Local::span(100, 202)})
+      ]));
 
   }
 }
